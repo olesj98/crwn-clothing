4a66b08b69e799a54cfdc68c7b168072646561a8
Oleksandr Chura <oleksandr.chura1@ibm.com>
2020-12-23 18:29:41 +0200

    add cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,12 @@ import { connect } from 'react-redux';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden }) => (
   <div className='header'>
     <Link to='/'>
       <Logo className='logo'/>
@@ -29,12 +32,17 @@ const Header = ({currentUser}) => (
           </Link>
         )
       }
+      <CartIcon />
     </div>
+    {
+      hidden ? null : (<CartDropdown />)
+    }
   </div>
 );
 
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
+  hidden: state.cart.hidden,
 })
 
 export default connect(mapStateToProps)(Header);
